4bb02925a2b16026c06906a5d11349ac52535606
tim.hawkins <tim.hawkins@redflaggroup.com>
2020-02-13 13:31:31 +0800

    Add Timeouts

--- src/Jira/Api/Client/CurlClient.php
+++ src/Jira/Api/Client/CurlClient.php
@@ -90,6 +90,9 @@ class CurlClient implements ClientInterface
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
 
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 30);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 30);
+
 		if ( !($credential instanceof Anonymous) ) {
 			curl_setopt($curl, CURLOPT_USERPWD, sprintf('%s:%s', $credential->getId(), $credential->getPassword()));
 		}
